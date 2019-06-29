77e1e3ba4e0d86a5fa9529c133720b5e54f67258
Ali Parandeh <43654026+Ali-Parandeh@users.noreply.github.com>
2019-06-29 01:12:34 +0100

    Added GPU Support

--- classifier.py
+++ classifier.py
@@ -2,13 +2,15 @@ import torch
 from torch import nn, optim
 from torchvision import datasets, transforms
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 transform = transforms.Compose([transforms.ToTensor(),  transforms.Normalize([0.5], [0.5])])
 
 trainset = datasets.MNIST("~/.pytorch/MNIST_data/", download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
-
+device = "cuda:0"
 
 model = nn.Sequential(nn.Linear(784, 128), nn.ReLU(), nn.Linear(128, 64), nn.ReLU(), nn.Linear(64, 10), nn.LogSoftmax(dim = 1))
 
@@ -16,9 +18,11 @@ criterion = nn.NLLLoss()
 optimizer = optim.SGD(model.parameters(), lr = 0.01)
 
 epochs = 5
+model = model.to(device)
 for e in range(epochs):
     running_loss = 0
     for images, lables in trainloader:
+        images, lables = images.to(device), lables.to(device)
         images = images.view(images.shape[0], -1)
         optimizer.zero_grad()
         output = model.forward(images)
